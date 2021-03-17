020bd83ded81f3f410f1232839c26f11a3f4a671
Oren Karaso <orenkaraso@gmail.com>
2021-03-17 16:29:21 +0200

    homepage wip

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,22 +1,29 @@
 <template>
-	<section class="home-page">
-		<h1>Hi</h1>
-		<board-list :boards="boards"/>
-	</section>
+  <section class="home-page">
+    <h1>Hi</h1>
+    <board-list :boards="boards" />
+    <el-card class="box-card">
+      <div v-for="o in 4" :key="o" class="text item">
+        {{ "List item " + o }}
+      </div>
+    </el-card>
+  </section>
 </template>
 
 <script>
 import boardList from "@/cmps/board-list";
 
 export default {
-	name: 'home-page',
-	computed: {
-		boards() {
-			return this.$store.getters.boards;
-		},
-	},
-	components: {
-		boardList
-	},
+  name: "home-page",
+  computed: {
+    boards() {
+      return this.$store.getters.boards;
+    },
+  },
+  components: {
+    boardList,
+  },
 };
 </script>
+
+
