530511c478377067ff8f8845990e87b03401d489
Gilad Peltz <gilad.peltz@gmail.com>
2021-03-17 13:56:42 +0200

    working on group

---
+++ src/cmps/color-picker.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="colors-container">
+    <button class="btn-color dark-green">
+    </button><button class="btn-color green">
+    </button><button class="btn-color khaki"></button><button class="btn-color orange"></button><button class="btn-color light-orange"></button><button class="btn-color yellow"></button><button class="btn-color purple"></button><button class="btn-color light-purple"></button><button class="btn-color blue"></button><button class="btn-color bourdeaux"></button><button class="btn-color red"></button><button class="btn-color pink"></button><button class="btn-color black"></button><button class="btn-color brown"></button><button class="btn-color gray"></button>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ src/cmps/group.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul class="group">
+    <div class="flex">
+
+      <el-dropdown
+        size="mini"
+        split-button="false"
+        type="primary"
+      >
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item @click="removeGroup">Delete Group</el-dropdown-item>
+          <el-dropdown-item @click="chooseColor">Change Color</el-dropdown-item>
+          
+        </el-dropdown-menu>
+      </el-dropdown>
+
+      <div>{{group.title}}</div>
+      <div>Members</div>
+      <div>Status</div>
+      <div>Timeline</div>
+      <div>Priority</div>
+    </div>
+    <li
+      v-for="task in tasks"
+      :key="task._id"
+      class="task-container"
+    >
+      <task-preview
+        :task="task"
+        :groupColor="groupColor"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+import taskPreview from "./task-preview.vue";
+
+export default {
+  props: {
+    group: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    remove(group) {
+      this.$emit("remove", group);
+    },
+  },
+  components: {
+    taskPreview,
+  },
+};
+</script>
