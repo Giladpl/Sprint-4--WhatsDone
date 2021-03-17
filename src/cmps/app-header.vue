591d0ba546b2307a96a0bf68d2d38d0ff3b0327c
Oren Karaso <orenkaraso@gmail.com>
2021-03-17 21:10:03 +0200

    header construction

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,12 +1,30 @@
 <template>
   <section class="header-component flex">
-    <div class="main-header flex .fd-column" />
-    <div class="secondary-header flex .fd-column" />
+    <div class="main-header flex">
+      <i class="el-icon-s-grid"></i>
+      <el-avatar
+        v-if="loggedInUser"
+        class="header-avatar"
+        size="medium"
+        :src="loggedInUser.imgUrl"
+      >
+      </el-avatar>
+      <el-avatar class="header-avatar" v-else size="medium">G</el-avatar>
+    </div>
+    <div class="secondary-header flex"/>
   </section>
 </template>
 
 <script>
 export default {
   name: "appHeader",
+  data() {
+    return {
+      loggedInUser: null,
+    };
+  },
+  created() {
+    this.$store.getters.loggedInUser = this.loggedInUser;
+  },
 };
 </script>
