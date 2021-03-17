2305810dde9765900b97c66fcbf3289e79551d0e
Oren Karaso <orenkaraso@gmail.com>
2021-03-17 18:27:06 +0200

    started to work on header

---
+++ src/cmps/app-header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header-component class="header-component flex">
+      <main-header class="main-header"/>
+      <secondary-header class="secondary-header"/>
+  </header-component>
+</template>
+
+<script>
+export default {
+
+}
+</script>
