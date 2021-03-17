965a790cd5d60866f74e2ba1e3edad742da68aae
zina <zinam84@gmail.com>
2021-03-17 09:52:39 +0200

    home-page & login

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { boardStore } from './board.store.js'
+import { userStore } from './user.store.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  strict: true,
   modules: {
+    boardStore,
+    userStore,
   }
 })
---
+++ src/views/home-page.vue
@@ -0,0 +1,17 @@
+<template>
+	<section class="home-page">
+		<h1>Hi</h1>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "home-page",
+	data() {
+		return {};
+	},
+	computed: {},
+	created() {},
+	methods: {},
+};
+</script>
