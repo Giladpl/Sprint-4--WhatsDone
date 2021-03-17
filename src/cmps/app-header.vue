6d491cb6130909d0889f1b6c60e31323df2401bd
Oren Karaso <orenkaraso@gmail.com>
2021-03-17 21:53:57 +0200

    sidebar

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="header-component flex">
     <div class="main-header flex">
-        <section class="top-icon-wrapper flex"> 
+        <!-- <section class="top-icon-wrapper flex">  -->
       <i class="header-icon el-icon-s-grid"></i>
       <i class="header-icon el-icon-bell"></i>
-        </section>
+        <!-- </section> -->
       <!-- <el-avatar v-if="loggedInUser" class="header-avatar" size="medium" :src="loggedInUser.imgUrl"> -->
-      </el-avatar>
+      <!-- </el-avatar> -->
       <el-avatar class="header-avatar" size="medium">G</el-avatar>
     </div>
     <div class="secondary-header flex"/>
