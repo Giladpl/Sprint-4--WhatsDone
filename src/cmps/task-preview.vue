365c679cbf22dc581d1cfe852beb74eeb2261bf8
zina <zinam84@gmail.com>
2021-03-17 12:53:43 +0200

    task

---
+++ src/cmps/task-preview.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="task-preview">
+        <div>
+            <div v-bind:style="{background: userColor}"></div>
+            <!-- <h4>{{task.title}}</h4> -->
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: {
+            toy: {
+                type: Object,
+                required: true
+            }
+        },
+        data() {
+            return {
+                groupColor = null
+            }
+        },
+        created() {
+            this.groupColor = this.task.color;
+        }
+    }
+</script>
