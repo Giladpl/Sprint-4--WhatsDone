30e2fde3d1db32191f57e2a0f942fef48009f8b2
zina <zinam84@gmail.com>
2021-03-17 14:44:44 +0200

    board data

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -13,11 +13,10 @@ export const boardService = {
     getEmptyToy: getEmptyBoard
 }
 
-// _createBoards() 
+_createBoards() 
 
-function query(filter) {
+function query() {
     // return httpService.get(`board/`)
-    // return httpService.get(`board?${filter}`)
     return asyncStorageService.query(KEY)
 }
 
@@ -55,18 +54,122 @@ function _createBoards() {
     let boards = storageService.load(KEY)
     if (!boards || !boards.length) {
         boards = [
-            _createBoard('toy1'),
-            _createBoard('toy2'),
-            _createBoard('toy3'),
+            _createBoard('b101', 'board1'),
+            _createBoard('b102', 'board2'),
+            _createBoard('b103', 'board3'),
         ]
         storageService.store(KEY, boards)
     }
     return boards;
 }
 
-function _createBoard(name) {
-    let board = getEmptyToy(name);
-    board._id = utilService.makeId();
-    return board;
+function _createBoard(_id, title) {
+    return {
+        _id,
+        title,
+        createdAt: 1589983468418,
+        createdBy: {
+            // Mini-user, ID from backend
+            _id: 'u101',
+            fullname: 'Abi Abambi',
+            imgUrl: 'http://some-img',
+        },
+        statuses: [
+            {
+                id: 's101',
+                title: 'Done',
+                color: '#61bd4f',
+            },
+            {
+                id: 's102',
+                title: 'Stuck',
+                color: 'red',
+            },
+            {
+                id: 's103',
+                title: 'Working on it',
+                color: 'yellow',
+            },
+        ],
+        priorities: [
+            {
+                id: 'p101',
+                title: 'High',
+                color: '#61bd4f',
+            },
+        ],
+        members: [
+            {
+                _id: 'u101', // Each is mini-user, ID from Backend
+                fullname: 'Tal Tarablus',
+                imgUrl: 'https://www.google.com',
+            },
+            {
+                _id: 'u102', // Each is mini-user, ID from Backend
+                fullname: 'Tal Tarablus',
+                imgUrl: 'https://www.google.com',
+            },
+        ],
+        groups: [
+            {
+                id: 'g101',
+                title: 'Group 1',
+                tasks: [
+                    {
+                        id: 't104',
+                        title: 'Help me',
+                        members: [
+                            {
+                                _id: 'u101',
+                                fullname: 'Tal Tarablus',
+                                imgUrl:
+                                    'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+                            },
+                        ],
+                        byMember: {
+                            _id: 'u102',
+                            fullname: 'Shraga Puk',
+                            imgUrl:
+                                'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+                        },
+                        comments: [
+                            {
+                                id: 'u101',
+                                update: 'changed font size',
+                                file: 'http://some-img',
+                                createdAt: 154514,
+                                byMember: {
+                                    _id: 'u101',
+                                    fullname: 'Abi Abambi',
+                                    imgUrl: 'http://some-img',
+                                },
+                            },
+                        ],
+                        statusId: 's101',
+                        priorityId: 'p102',
+                        createdAt: 1590999730348,
+                        dueDate: 16156215211,
+                    },
+                ],
+                color: '#8D0000',
+            },
+        ],
+        activities: [
+            {
+                id: 'a101',
+                action: 'Changed Color',
+                createdAt: 154514,
+                byMember: {
+                    _id: 'u101',
+                    fullname: 'Abi Abambi',
+                    imgUrl: 'http://some-img',
+                },
+                task: {
+                    id: 't101',
+                    title: 'Replace Logo',
+                },
+            },
+        ],
+    };
 }
 
