69c5e04bb964ce9c2bc4c523bafcd3ce6f189c54
Oren Karaso <orenkaraso@gmail.com>
2021-03-18 13:42:20 +0200

    user-profile cpmponent

---
+++ src/views/user-profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+      <div class="top">
+          <el-avatar v-if="loggedInUser" class="usr-profile-avatar" size="large" :src="loggedInUser.imgUrl">{{checkLoggedUser}}</el-avatar>
+          <el-avatar v-else class="usr-profile-avatar" size="large">{{checkLoggedUser}}</el-avatar>
+          <h1 :checkLoggedUser="checkLoggedUser">{{checkLoggedUser}}</h1>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "user-profile",
+  data() {
+    return {
+      loggedInUser: null,
+    };
+  },
+  computed: {
+    checkLoggedUser() {
+      if (!this.loggedInUser) return "Guest";
+      else return this.loggedInUser.fullname;
+    },
+  },
+  created() {
+    const user = JSON.parse(this.$store.getters.loggedInUser) || null;
+    this.loggedInUser = user || null;
+  },
+};
+</script>
+
