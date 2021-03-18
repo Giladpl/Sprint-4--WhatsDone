187345c24d8b4345ac13733c3bef2f25627d9271
zina <zinam84@gmail.com>
2021-03-18 19:25:35 +0200

    side header

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,15 +1,19 @@
 <template>
   <section class="header-component flex">
     <div class="main-header flex">
-      <i><img class="logo" src="@/assets/icons/increase.svg"/></i>
-      <i class="header-icon el-icon-s-grid"></i>
-      <i class="header-icon el-icon-bell"></i>
-      <router-link to="/profile">
-        <el-avatar class="header-avatar" size="medium">G</el-avatar>
-      </router-link>
-      <i class="fas fa-sign-out-alt"></i>
+      <div class="top-header flex">
+        <i><img class="logo" src="@/assets/icons/increase.svg"/></i>
+        <i class="header-icon el-icon-s-grid"></i>
+        <i class="header-icon el-icon-bell"></i>
+      </div>
+      <div class="bottom-header flex">
+        <router-link to="/profile">
+          <el-avatar class="header-avatar" size="medium">G</el-avatar>
+        </router-link>
+        <i><img class="exit" src="@/assets/icons/exit.svg"/></i>
+      </div>
     </div>
-    <div class="secondary-header flex" />
+    <!-- <div class="secondary-header flex" /> -->
   </section>
 </template>
 
