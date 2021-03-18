5dcf67c8e74d8f4d7bd7a5bc57370e08d61f9d67
Oren Karaso <orenkaraso@gmail.com>
2021-03-18 11:11:13 +0200

    uniicons npm

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -2,12 +2,15 @@
   <section class="header-component flex">
     <div class="main-header flex">
         <!-- <section class="top-icon-wrapper flex">  -->
+          <i><img src="@/assets/icons/logo.jpg"></i>
+          <i><img src="@/assets/icons/calendar.svg"></i>
       <i class="header-icon el-icon-s-grid"></i>
       <i class="header-icon el-icon-bell"></i>
         <!-- </section> -->
       <!-- <el-avatar v-if="loggedInUser" class="header-avatar" size="medium" :src="loggedInUser.imgUrl"> -->
       <!-- </el-avatar> -->
       <el-avatar class="header-avatar" size="medium">G</el-avatar>
+      <i class="uis uis-signout"></i>
     </div>
     <div class="secondary-header flex"/>
   </section>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,17 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import Unicon from 'vue-unicons/dist/vue-unicons-vue2.umd';
+import { uniLayerGroupMonochrome, uniCarWash } from 'vue-unicons/dist/icons';
 
 import './styles/styles.scss';
 
 Vue.use(ElementUI);
+
+Unicon.add([uniLayerGroupMonochrome, uniCarWash]);
+Vue.use(Unicon);
+
+
 
 
 Vue.config.productionTip = false
