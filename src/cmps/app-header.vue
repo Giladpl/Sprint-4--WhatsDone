0e2337fbc62268dda5d024c4271fe7c30a1cd075
zina <zinam84@gmail.com>
2021-03-18 18:39:21 +0200

    css

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,19 +1,12 @@
 <template>
   <section class="header-component flex">
     <div class="main-header flex">
-      <!-- <section class="top-icon-wrapper flex">  -->
-      <i><img src="@/assets/icons/logo.jpg" /></i>
-      <i><img src="@/assets/icons/calendar.svg" /></i>
+      <i><img class="logo" src="@/assets/icons/increase.svg"/></i>
       <i class="header-icon el-icon-s-grid"></i>
       <i class="header-icon el-icon-bell"></i>
-      <!-- </section> -->
-      <!-- <el-avatar v-if="loggedInUser" class="header-avatar" size="medium" :src="loggedInUser.imgUrl"> -->
-      <!-- </el-avatar> -->
-      <router-link to="/profile"
-        ><el-avatar class="header-avatar" size="medium"
-          >G</el-avatar
-        ></router-link
-      >
+      <router-link to="/profile">
+        <el-avatar class="header-avatar" size="medium">G</el-avatar>
+      </router-link>
       <i class="fas fa-sign-out-alt"></i>
     </div>
     <div class="secondary-header flex" />
