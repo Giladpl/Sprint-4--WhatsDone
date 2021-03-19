fbc36c8526900e4342c942f0510eb03924509a1c
Gilad Peltz <gilad.peltz@gmail.com>
2021-03-19 09:46:08 +0200

    found non-duplicated members in taskMembers cmp

--- src/cmps/task-members.vue
+++ src/cmps/task-members.vue
@@ -1,12 +1,19 @@
 <template>
   <section class="task-members flex">
-      <ul class="task-members-list clean-list flex">
-        <li v-for="member in taskMembers" :key="member._id" class="task-member flex">
-          <el-avatar size="small" :src="member.imgUrl"></el-avatar>
-          <p class="task-member-name">{{member.fullname}}</p>
-          <i class="el-icon-minus"></i>
-        </li>
-      </ul>
+    <ul class="task-members-list clean-list flex">
+      <li
+        v-for="member in boardMembers"
+        :key="member._id"
+        class="task-member flex"
+      >
+        <el-avatar
+          size="small"
+          :src="member.imgUrl"
+        ></el-avatar>
+        <p class="task-member-name">{{member.fullname}}</p>
+        <!-- <i class="el-icon-plus"></i> -->
+      </li>
+    </ul>
   </section>
 </template>
 
@@ -23,8 +30,27 @@ export default {
     },
   },
   name: "task-members",
-  data() {return{}},
+  data() {
+    return {
+      taskMembersIds: [],
+      nonDuplicatedMembers: [],
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
   created() {
+    this.taskMembersIds = this.taskMembers.reduce((acc, member) => {
+      acc.push(member._id)
+      return acc
+    }, [])
+    const members = this.boardMembers.filter(member => {
+      if (!this.taskMembersIds.includes(member._id)) return member
+    })
+    console.log(members);
   },
 };
 </script>
