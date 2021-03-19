1b4db75d2e8ed0c8cd5c5ed840c0925bce727d35
zina <zinam84@gmail.com>
2021-03-19 10:31:40 +0200

    status modal

---
+++ src/cmps/task-status.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="status-modal">
+        <ul v-if="renderStatuses">
+            <li v-for="status in renderStatuses" :key="status.id">
+                <div v-bind:style="{ borderLeft: `10px solid ${status.color}` }">{{status.title}}</div>
+            </li>
+            <li>
+                <input 
+                ref="addStatusInput"
+                @keyup.enter="addStatus"
+                class="input-add-status"
+                type="text"
+                placeholder="New label"
+                v-bind:style="{ borderLeft: `10px solid ${addStatusColor}` }"
+                >
+            </li>
+        </ul>
+        <color-picker @changeColor="changeColor"/>
+    </section>
+</template>
+
+<script>
+import colorPicker from "./color-picker";
+export default {
+    statuses: {
+      type: Array,
+      required: true,
+    },
+    data() {
+        return {
+            renderStatuses: [],
+            addStatusColor: 'gray'
+        }
+    },
+    methods: {
+        changeColor(color) {
+            this.addStatusColor = color;
+        },
+        addStatus(ev) {
+            this.$emit('addStatus', { title: ev.target.value, color: this.addStatusColor })
+            this.$refs['addStatusInput'].value = ''
+        },
+    },
+    created() {
+        this.renderStatuses = this.statuses.filter(status => status.id !== 'sNew')
+    },
+    components: {
+        colorPicker
+    },
+}
+</script>
