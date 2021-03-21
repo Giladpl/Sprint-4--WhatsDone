e8e5980cf1c4ec6c7c150944684635e4a99a5b03
Oren Karaso <orenkaraso@gmail.com>
2021-03-21 12:47:14 +0200

    span added in homepage

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -9,7 +9,7 @@
             <h1>WhatsDone</h1>
             <p>From the initial idea to the end product. Manage you project with your team members
                  by defining tasks, assign different responsibilities and check their statuses.
-                 WhatsDone will help you make sure you are in control and on top of everyting!.</p>
+                 <span>WhatsDone</span> will help you make sure you are in control and on top of everyting!.</p>
             <router-link class="try-btn" to="/board">Try now!</router-link>
         </div>
     </div>
