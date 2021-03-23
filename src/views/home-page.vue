f25334e5b91b97ed62121fdedcb66f08e904f04f
zina <zinam84@gmail.com>
2021-03-23 20:07:01 +0200

    homepage css

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -7,9 +7,10 @@
         </div>
         <div>
             <h1>WhatsDone</h1>
-            <p>From the initial idea to the end product. Manage you project with your team members
-                 by defining tasks, assign different responsibilities and check their statuses.
-                 <span>WhatsDone</span> will help you make sure you are in control and on top of everyting!.</p>
+            <p class="home-description">From the initial idea to the end product. Manage you project with your team members
+                by defining tasks, assign different responsibilities and check their statuses.
+                <span>WhatsDone</span> will help you make sure you are in control and on top of everyting!.
+            </p>
             <router-link class="try-btn" to="/board">Try now!</router-link>
         </div>
     </div>
