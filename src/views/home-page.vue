c9c28d8b741ee8f460b6f657092a751d329dff64
zina <zinam84@gmail.com>
2021-03-24 16:50:34 +0200

    homepage css & fonts

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,18 +1,24 @@
 <template>
   <section class="home-page">
-    <home-page-header />
-    <div class="main-content">
-        <div class="logo">
-            <img src="@/assets/icons/increase.svg" width="100">
+    <div class="main-home-page">
+        <home-page-header />
+        <div class="main-content">
+            <div class="co-working-img">
+                <img src="@/assets/icons/co-working.svg">
+            </div>
+            <div>
+                <h1>WhatsDone</h1>
+                <p class="home-description">From the initial idea to the end product. Manage you project with your team members
+                    by defining tasks, assign different responsibilities and check their statuses.
+                    <span>WhatsDone</span> will help you make sure you are in control and on top of everyting!.
+                </p>
+                <router-link class="try-btn" to="/board">Try now!</router-link>
+            </div>
         </div>
-        <div>
-            <h1>WhatsDone</h1>
-            <p class="home-description">From the initial idea to the end product. Manage you project with your team members
-                by defining tasks, assign different responsibilities and check their statuses.
-                <span>WhatsDone</span> will help you make sure you are in control and on top of everyting!.
-            </p>
-            <router-link class="try-btn" to="/board">Try now!</router-link>
-        </div>
+
+    </div>
+    <div class="side-bar">
+
     </div>
   </section>
 </template>
