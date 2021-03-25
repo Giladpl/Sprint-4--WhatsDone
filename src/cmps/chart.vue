01e42891b3afdaac9f4b5c78632ea61d750373d0
Gilad Peltz <gilad.peltz@gmail.com>
2021-03-25 09:24:45 +0200

    added grid to tables

--- src/cmps/chart.vue
+++ src/cmps/chart.vue
@@ -1,17 +1,15 @@
 <template>
   <section class="charts-main-container flex">
-    <div class="upper-charts flex">
-      <div class="priority-chart">
-        <h2>Board Priorities by Percentages:</h2>
-        <chartPriorityTotal class="chart chart-priority" />
-      </div>
-      <div class="status-chart">
-        <h2>Board Statuses by Percentages:</h2>
-        <chartStatusTotal class="chart chart-status" />
-      </div>
+    <div class="priority-chart">
+      <h2>Board Priorities by Percentages:</h2>
+      <chartPriorityTotal class="chart chart-priority" />
+    </div>
+    <div class="status-chart">
+      <h2>Board Statuses by Percentages:</h2>
+      <chartStatusTotal class="chart chart-status" />
     </div>
     <div class="lower-chart">
-      <h2>Task per Hour:</h2>
+      <h2>Total Work Time per Task:</h2>
       <chartTimePerTask class="chart chart-time" />
     </div>
   </section>
