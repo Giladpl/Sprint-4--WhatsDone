6d69582dcebaf637a687d7c04d03f63e1317f03a
zina <zinam84@gmail.com>
2021-03-25 16:25:24 +0200

    homepage css

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,18 +1,20 @@
 <template>
   <section class="home-page">
-    <div class="hero-homepage">
-        <home-page-header />
-        <div class="main-hero flex-between">
-            <div class="hero-content">
-                <h1 class="title-hero">Project management made simple.</h1>
-                <p class="txt-hero">
-                    From the initial idea to the end product. Manage you project with your team members
-                    by defining tasks, assigning responsibilities and folowing-up with their statuses.
-                </p>
-                <router-link class="try-btn" to="/board">Try now!</router-link>
-            </div>
-            <div class="img-hero">
-                <img src="@/assets/icons/scrum-board.svg">
+    <div class="hero-container">
+        <div class="hero-homepage">
+            <home-page-header />
+            <div class="main-hero flex-between">
+                <div class="hero-content">
+                    <h1 class="title-hero">Project management made simple.</h1>
+                    <p class="txt-hero">
+                        From the initial idea to the end product. Manage you project with your team members
+                        by defining tasks, assigning responsibilities and folowing-up with their statuses.
+                    </p>
+                    <router-link class="try-btn" to="/board">Try now!</router-link>
+                </div>
+                <div class="img-hero">
+                    <img src="@/assets/icons/collaboration.svg">
+                </div>
             </div>
         </div>
     </div>
